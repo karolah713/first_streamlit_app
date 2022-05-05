92debe69b01ca3b0d7e3fbe1df8d17d6868175a9
Karolina Haponik <45128359+karolah713@users.noreply.github.com>
2022-05-05 11:24:00 +0200

    Update streamlit_app.py
    Fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
   fruitvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + this_fruit_choice)
   fruitvice_normalized = pandas.json_normalize(fruitvice_response.json())
-  returned fruitvice_normalized
+  return fruitvice_normalized
 
 #NewcSelection to display fruitvice api response
 streamlit.header('Fruitvice Fruit Advice!')
