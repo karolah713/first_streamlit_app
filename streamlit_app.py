a5b6f88813bf0a51a9c0d6ce9518ed147f4f0c44
Karolina Haponik <45128359+karolah713@users.noreply.github.com>
2022-05-05 10:29:05 +0200

    Update streamlit_app.py
    Add A Second Text Entry Box

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# Allow the end user to add a fruit to the list
+add_my_ftuit = streamlit.text_input('What fruit would you like to add?')
+streamlit.text('Thanka for adding ' + add_my_ftuit)
+
+
