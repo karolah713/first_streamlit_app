2ec2fc5f3cda5cb4e01c12b79ad5b46e19ccca6a
Karolina Haponik <45128359+karolah713@users.noreply.github.com>
2022-05-05 11:51:55 +0200

    Update streamlit_app.py
    Fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
     return "Thanks forvadding " + new_fruit
   
-add_my_ftuit = streamlit.text_input('What fruit would you like to add?')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to rhe List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
